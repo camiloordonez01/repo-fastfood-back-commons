4e480e95ed0389d7d0714e579ecf7cd0b39ee9ac
Camilo Ordoñez <orarjuan@hotmail.com>
2022-05-26 13:28:03 -0500

    Update messages and database file

--- infrastructure/database/index.js
+++ infrastructure/database/index.js
@@ -1,17 +1,18 @@
 const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize({
-    dialect: 'postgres',
+    dialect: 'mysql',
     logging: false,
     dialectOptions: { decimalNumbers: true, supportBigNumbers: true },
     hooks: {
         beforeConnect: async (config) => {
             const newConfig = config
 
-            const { HOST_DB, PORT_DB, USER_DB, PASSWORD_DB, SCHEMA_DB } = process.env
+            const { HOST_DB, PORT_DB, USER_DB, PASSWORD_DB, SCHEMA_DB } =
+                process.env
 
             newConfig.host = HOST_DB
-            newConfig.post = PORT_DB
+            newConfig.port = PORT_DB
             newConfig.username = USER_DB
             newConfig.password = PASSWORD_DB
             newConfig.database = SCHEMA_DB
